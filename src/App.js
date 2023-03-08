846368d6797972861e379ccefc53b204c488d0e3
Rupak shrestha <rupakshrestha2002@gmail.com>
2023-03-08 11:32:26 +0545

    Add reveal animation.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
 import { Nav, Home, Service, Team, Footer, Contact } from './components';
+import { Fade } from 'react-awesome-reveal';
 import './index.css';
 const App = () => {
   return (
     <div className="background ">
       <Nav />
-      <Home />
-      <Service />
-      <Team />
+      <Fade duration={3000}>
+        <Home />
+      </Fade>
+      <Fade duration={3000}>
+        <Service />
+      </Fade>
+      <Fade duration={3000}>
+        <Team />
+      </Fade>
+      <Fade duration={3000}>
+        <Contact />
+      </Fade>
 
-      <Contact />
       <Footer />
     </div>
   );
--- src/components/index.js
+++ src/components/index.js
@@ -3,5 +3,4 @@ export { default as Home } from './Home/Home';
 export { default as Service } from './Service/Service';
 export { default as Team } from './Team/Team';
 export { default as Footer } from './Footer/Footer';
-export { default as Faq } from './Faq/Faq';
 export { default as Contact } from './Contact/Contact';
